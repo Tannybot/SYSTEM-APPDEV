0818fa217d005dabfdaa65cfa89719df4047dfe1
Tannybot <youremail@example.com>
2025-12-03 15:01:55 +0800

    new pages

--- admin/faculty.php
+++ admin/faculty.php
@@ -87,7 +87,7 @@
                 </tr>
                 <tr class="menu-row" >
                     <td class="menu-btn menu-icon-patient">
-                        <a href="patient.php" class="non-style-link-menu"><div><p class="menu-text">Patients</p></a></div>
+                        <a href="student.php" class="non-style-link-menu"><div><p class="menu-text">Students</p></a></div>
                     </td>
                 </tr>
 
---
+++ faculty-signup.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/animations.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/signup.css">
+
+    <title>Faculty Sign Up</title>
+    <style>
+        .container{
+            animation: transitionIn-X 0.5s;
+        }
+    </style>
+</head>
+<body>
+<?php
+
+//learn from w3schools.com
+//Unset all the server side variables
+
+session_start();
+
+$_SESSION["user"]="";
+$_SESSION["usertype"]="";
+
+// Set the new timezone
+date_default_timezone_set('Asia/Kolkata');
+$date = date('Y-m-d');
+
+$_SESSION["date"]=$date;
+
+//import database
+include("connection.php");
+
+if($_POST){
+
+    $result= $database->query("select * from webuser");
+
+    $fname=$_POST['fname'];
+    $lname=$_POST['lname'];
+    $name=$fname." ".$lname;
+    $email=$_POST['newemail'];
+    $tele=$_POST['tele'];
+    $newpassword=$_POST['newpassword'];
+    $cpassword=$_POST['cpassword'];
+    $nic=$_POST['nic'];
+    $specialties=$_POST['specialties'];
+
+    if ($newpassword==$cpassword){
+        $sqlmain= "select * from webuser where email=?;";
+        $stmt = $database->prepare($sqlmain);
+        $stmt->bind_param("s",$email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if($result->num_rows==1){
+            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Already have an account for this Email address.</label>';
+        }else{
+            //Insert into faculty table
+            $database->query("insert into faculty(facemail,facname,facpassword, facnic,factel,specialties) values('$email','$name','$newpassword','$nic','$tele',$specialties);");
+            $database->query("insert into webuser values('$email','f')");
+
+            $_SESSION["user"]=$email;
+            $_SESSION["usertype"]="f";
+            $_SESSION["username"]=$fname;
+
+            header('Location: faculty/index.php');
+            $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;"></label>';
+        }
+
+    }else{
+        $error='<label for="promter" class="form-label" style="color:rgb(255, 62, 62);text-align:center;">Password Confirmation Error! Reconfirm Password</label>';
+    }
+
+}else{
+    $error='<label for="promter" class="form-label"></label>';
+}
+
+?>
+
+
+    <center>
+    <div class="container">
+        <table border="0" style="width: 69%;">
+            <tr>
+                <td colspan="2">
+                    <p class="header-text">Faculty Sign Up</p>
+                    <p class="sub-text">Create Your Faculty Account.</p>
+                </td>
+            </tr>
+            <tr>
+                <form action="" method="POST" >
+                <td class="label-td" colspan="2">
+                    <label for="name" class="form-label">Name: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td">
+                    <input type="text" name="fname" class="input-text" placeholder="First Name" required>
+                </td>
+                <td class="label-td">
+                    <input type="text" name="lname" class="input-text" placeholder="Last Name" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="nic" class="form-label">NIC: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="text" name="nic" class="input-text" placeholder="NIC" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="newemail" class="form-label">Email: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="email" name="newemail" class="input-text" placeholder="Email Address" required>
+                </td>
+
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="tele" class="form-label">Mobile Number: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                <input type="tel" name="tele" class="input-text"  placeholder="ex. 0712345678" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="specialties" class="form-label">Specialties: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <select name="specialties" class="input-text" required>
+                        <option value="" disabled selected hidden>Choose Specialty</option>
+                        <?php
+                        $list11 = $database->query("select * from specialties order by sname asc;");
+                        for ($y=0;$y<$list11->num_rows;$y++){
+                            $row00=$list11->fetch_assoc();
+                            $sn=$row00["sname"];
+                            $id00=$row00["id"];
+                            echo "<option value=".$id00.">$sn</option>";
+                        };
+                        ?>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="newpassword" class="form-label">Create New Password: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="password" name="newpassword" class="input-text" placeholder="New Password" required>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <label for="cpassword" class="form-label">Confirm Password: </label>
+                </td>
+            </tr>
+            <tr>
+                <td class="label-td" colspan="2">
+                    <input type="password" name="cpassword" class="input-text" placeholder="Confirm Password" required>
+                </td>
+            </tr>
+
+            <tr>
+
+                <td colspan="2">
+                    <?php echo $error ?>
+
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >
+                </td>
+                <td>
+                    <input type="submit" value="Sign Up" class="login-btn btn-primary btn">
+                </td>
+
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <br>
+                    <label for="" class="sub-text" style="font-weight: 280;">Already have an account&#63; </label>
+                    <a href="login.php" class="hover-link1 non-style-link">Login</a>
+                    <br><br><br>
+                </td>
+            </tr>
+
+                    </form>
+            </tr>
+        </table>
+
+    </div>
+</center>
+</body>
+</html>
